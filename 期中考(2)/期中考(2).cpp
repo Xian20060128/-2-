32e8d0d5207ac38249dcac9da598c07fb3805c56
Xian20060128 <4B3G0050@office.stust.edu.tw>
2024-11-13 14:13:40 +0800

    0

--- 期中考(2)/期中考(2).cpp
+++ 期中考(2)/期中考(2).cpp
@@ -27,7 +27,7 @@ int main()
         sum = 0;
         do {
             
-                sum += i; i += 2;
+               sum += i; i += 2;
         } while (i <= n);
         cout << "Do-While 1oop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
 
